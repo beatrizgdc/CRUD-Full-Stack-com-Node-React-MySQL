6cde3261cc498afff0b5c3c806dc15a6d625a99e
Beatriz Garcia <101670962+beatrizgdc@users.noreply.github.com>
2024-07-27 22:04:31 +0000

    portbd

--- api/conexao.js
+++ api/conexao.js
@@ -2,6 +2,7 @@ import mysql from "mysql";
 
 export const conexao = mysql.createConnection({
     host: "localhost",
+    port: 3306, 
     user: "root",
     password: "123456",
     database: "crud_nodejs",
