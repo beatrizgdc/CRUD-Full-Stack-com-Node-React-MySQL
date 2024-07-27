28fb005234593fe017cca49461e543f57c9fec86
Beatriz Garcia <101670962+beatrizgdc@users.noreply.github.com>
2024-07-27 22:02:26 +0000

    teste

--- api/index.js
+++ api/index.js
@@ -10,6 +10,6 @@ app.use(cors());
 app.use("/", userRoutes);
 
 const PORT = process.env.PORT || 8800;
-app.listen(PORT, () => {
+app.listen(PORT, '127.0.0.1', () => {
   console.log(`Server running on port ${PORT}`);
 });
