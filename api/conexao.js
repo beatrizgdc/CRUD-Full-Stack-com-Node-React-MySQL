84a84d4563b945738f98f8588a7cb59d42ae9b7e
Beatriz Garcia <101670962+beatrizgdc@users.noreply.github.com>
2024-07-27 20:35:11 +0000

    teste

--- api/conexao.js
+++ api/conexao.js
@@ -5,4 +5,12 @@ export const conexao = mysql.createConnection({
     user: "root",
     password: "123456",
     database: "crud_nodejs",
-})
+});
+
+conexao.connect(err => {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados:', err);
+        return;
+    }
+    console.log('Conectado ao banco de dados.');
+});
