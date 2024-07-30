24a22c9c2417d5c1e556f5724327ab8f7122a7a6
Beatriz Garcia <101670962+beatrizgdc@users.noreply.github.com>
2024-07-30 14:18:44 -0300

    revisao de cod

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,9 +1,9 @@
-import { conexao } from "../conexao.js";
+import { db } from "../db.js";
 
 export const getUsers = (_, res) => {
     const q = "SELECT * FROM usuarios";
 
-    conexao.query(q, (err, data) => {
+    db.query(q, (err, data) => {
         if (err) return res.json(err);
 
         return res.status(200).json(data);
--- api/db.js
+++ api/db.js
@@ -1,9 +1,9 @@
 import mysql from "mysql";
 
-export const conexao = mysql.createConnection({
+export const db = mysql.createConnection({
   host: 'localhost', 
   port: 3306,        
   user: 'root',      
   password: '123456', 
-  database: 'nome_do_banco',
+  database: 'crud',
 });
