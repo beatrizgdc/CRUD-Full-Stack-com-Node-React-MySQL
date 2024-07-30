30beeb00a493359bf4467826808b8dd892e9cb7d
Beatriz Garcia <101670962+beatrizgdc@users.noreply.github.com>
2024-07-30 19:40:31 +0000

    teste

--- api/controllers/users.mjs
+++ api/controllers/users.mjs
@@ -1,14 +1,11 @@
-import { mysqlConfig } from '../db.mjs';
+import mysqlConfig from '../db.mjs';  
 
-export const getUsers = (_, res) => {
-  const q = "SELECT * FROM usuarios";
-
-  mysqlConfig.query(q, (err, data) => {
-    if (err) {
-      console.error('Error executing query:', err);
-      return res.status(500).json({ error: 'Error executing query' });
-    }
-
-    return res.status(200).json(data);
-  });
+export const getUsers = async (_, res) => {
+  try {
+    const [rows] = await mysqlConfig.query('SELECT * FROM usuarios');
+    res.status(200).json(rows);
+  } catch (err) {
+    console.error('Error executing query:', err);
+    res.status(500).json({ error: 'Error executing query' });
+  }
 };
--- api/index.mjs
+++ api/index.mjs
@@ -6,18 +6,8 @@ import userRoutes from './routes/users.mjs';
 const app = express();
 const server = http.createServer(app);
 
-// Configura o Express para interpretar JSON
 app.use(express.json());
 
-// Define a rota raiz (opcional)
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-// Usa o roteador de usuários para a rota /api/users
 app.use('/api/routes', userRoutes);
 
-// Inicia o servidor na porta 3000
-server.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
+server.listen();
