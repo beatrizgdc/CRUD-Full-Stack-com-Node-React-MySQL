8db4f7e3410c7357c28b04394a037ddfb7dbcee1
Beatriz Garcia <101670962+beatrizgdc@users.noreply.github.com>
2024-07-31 14:52:55 +0000

    teste

--- api/db.mjs
+++ api/db.mjs
@@ -1,9 +1,32 @@
 import mysql from 'mysql2/promise';
 
-export const mysqlConfig = mysql.createPool({
-  connectionLimit: 10,
-  host: 'localhost',
-  user: 'root',
-  password: '123456',
-  database: 'crud',
-});
+const createPool = () => {
+  try {
+    console.log('Tentando criar uma pool de conexões com o banco de dados...');
+    const pool = mysql.createPool({
+      connectionLimit: 10,
+      host: 'localhost',
+      user: 'root',
+      password: '123456',
+      database: 'crud',
+    });
+
+    // Testar a conexão
+    pool.getConnection()
+      .then((connection) => {
+        console.log('Conexão com o banco de dados estabelecida com sucesso.');
+        connection.release(); // Liberar a conexão de volta ao pool
+      })
+      .catch((err) => {
+        console.error('Erro ao conectar com o banco de dados:', err);
+      });
+
+    return pool;
+  } catch (err) {
+    console.error('Erro ao criar a pool de conexões:', err);
+    throw err;
+  }
+};
+
+// Exportar a pool de conexões
+export const mysqlConfig = createPool();
